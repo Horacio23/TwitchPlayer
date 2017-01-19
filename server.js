f14ae1d8a93612b61a81a431fd305b86dcba1cb6
Horacio <horacio@Horacios-MBP.hsd1.fl.comcast.net>
2017-01-19 10:06:46 -0500

    added a small example of the google assistant

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 'use strict';
 
-const express = require('express');
+process.env.DEBUG = 'actions-on-google:*';
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.json({type: 'application/json'}));
 
 // Constants
 const PORT = 8080;
 
-// App
-const app = express();
-app.get('/', function (req, res) {
-console.log("request received")
-  res.send('Que bola locol');
+// [START YourAction]
+app.post('/', function (req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Fulfill action business logic
+  function responseHandler (assistant) {
+    // Complete your fulfillment logic and send a response
+    assistant.tell('Hello, World!');
+  }
+
+  assistant.handleRequest(responseHandler);
 });
+// [END YourAction]
 
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
 
-app.post('/play', function (req, res) {
-console.log("Post received")
-  res.send('Que bola locol');
-});
-
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+module.exports = app;
